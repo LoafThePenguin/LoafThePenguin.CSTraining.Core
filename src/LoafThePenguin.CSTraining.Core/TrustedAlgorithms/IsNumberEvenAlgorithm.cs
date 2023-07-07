c5307aefd48e0a95ccc3417563b1999f2a5efeba
Екатерина <kathyaska@mail.ru>
2023-07-08 01:11:23 +0300

    Добавление алгоритмов.
    - Алгоритм определения чётного числа.
    - Алгоритм определения простого числа

---
+++ src/LoafThePenguin.CSTraining.Core/TrustedAlgorithms/IsNumberEvenAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿namespace LoafThePenguin.CSTraining.Core.TrustedAlgorithms
+{
+    public sealed class IsNumberEvenAlgorithm : IAlgorithm<int, bool>
+    {
+        public bool Run(int input)
+        {
+            return input % 2 == 0;
+        }
+    }
+}
---
+++ src/LoafThePenguin.CSTraining.Core/TrustedAlgorithms/IsNumberSimpleAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿namespace LoafThePenguin.CSTraining.Core.TrustedAlgorithms
+{
+    public sealed class IsNumberSimpleAlgorithm : IAlgorithm<int, bool>
+    {
+        public bool Run(int input)
+        {
+            for (int divider = 2; divider < input / 2; divider++)
+            {
+                if (input % divider == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
