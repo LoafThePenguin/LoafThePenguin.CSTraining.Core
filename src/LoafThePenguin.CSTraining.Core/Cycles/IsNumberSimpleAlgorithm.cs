87eb4de2ae1bdd51c125b72b075c6c7f72b8df9d
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-12 00:02:32 +0500

    Исправление алгоритма определения простого числа.

--- src/LoafThePenguin.CSTraining.Core/Cycles/IsNumberSimpleAlgorithm.cs
+++ src/LoafThePenguin.CSTraining.Core/Cycles/IsNumberSimpleAlgorithm.cs
@@ -22,7 +22,7 @@ public sealed class IsNumberSimpleAlgorithm : IAlgorithm<int, bool>
     /// <returns><see langword="true"/>, если число простое. Иначе - <see langword="false"/>.</returns>
     public bool Run(int input)
     {
-        for (int divider = 2; divider < input / 2; divider++)
+        for (int divider = 2; divider <= input / 2; divider++)
         {
             if (input % divider == 0)
             {
