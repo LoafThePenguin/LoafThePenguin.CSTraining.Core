5776ac436b0a9103ca7f7eb2e9297f5ab6d1f163
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-12 20:33:44 +0500

    Удаление консольных приложений

--- console/LoafThePenguin.CSTraining.AllSimpleNumbersToIntMaxValueAlgorithm/Program.cs
+++
@@ -1,9 +0,0 @@
-using LoafThePenguin.CSTraining.Core.TrustedAlgorithms.Cycles;
-
-AllSimpleNumbersToIntMaxValueAlgorithm algorithm = new();
-IEnumerable<int> simpleNumbers = algorithm.Run(10);
-
-foreach (int number in simpleNumbers.OrderBy(x => x))
-{
-    Console.WriteLine(number);
-}
