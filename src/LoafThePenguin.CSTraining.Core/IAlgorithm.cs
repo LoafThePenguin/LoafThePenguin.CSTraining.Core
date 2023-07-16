dfa4cc1c8191606e9fea7b46245420ca77f74038
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-16 21:16:34 +0500

    Добавлен интерфейс алгоритма с двумя входными параметрами.

--- src/LoafThePenguin.CSTraining.Core/IAlgorithm.cs
+++ src/LoafThePenguin.CSTraining.Core/IAlgorithm.cs
@@ -18,6 +18,23 @@ public interface IAlgorithm<in TInput, out TOutput>
 /// <summary>
 /// Базовый интерфейс алгоритма.
 /// </summary>
+/// <typeparam name="TInput1">Тип входного значения 1.</typeparam>
+/// <typeparam name="TInput2">Тип входного значения 2.</typeparam>
+/// <typeparam name="TOutput">Тип выходного значения.</typeparam>
+public interface IAlgorithm<in TInput1, in TInput2, out TOutput>
+{
+    /// <summary>
+    /// Запускает алгоритм.
+    /// </summary>
+    /// <param name="input1">Входное значение 1.</param>
+    /// <param name="input2">Входное значение 2.</param>
+    /// <returns>Экземпляр выходного значения.</returns>
+    TOutput Run(TInput1 input1, TInput2 input2);
+}
+
+/// <summary>
+/// Базовый интерфейс алгоритма.
+/// </summary>
 /// <typeparam name="TOutput">Тип выходного значения.</typeparam>
 public interface IAlgorithm<out TOutput>
 {
