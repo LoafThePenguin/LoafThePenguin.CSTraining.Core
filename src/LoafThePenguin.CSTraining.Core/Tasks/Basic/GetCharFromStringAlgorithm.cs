55debbf9ee74610bd2d05dd7afab26ddbf0dbaca
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-18 21:18:29 +0500

    Добавление алгоритма определения буквы строки по индексу.

---
+++ src/LoafThePenguin.CSTraining.Core/Tasks/Basic/GetCharFromStringAlgorithm.cs
@@ -0,0 +1,46 @@
+using LoafThePenguin.Helpers;
+
+namespace LoafThePenguin.CSTraining.Core.Tasks.Basic;
+
+/// <summary>
+/// Алгоритм получения буквы строки.
+/// </summary>
+public sealed class GetCharFromStringAlgorithm : IAlgorithm<string, int, char?>
+{
+    /// <summary>
+    /// Создаёт экземпляр алгоритма получения буквы строки.
+    /// </summary>
+    public GetCharFromStringAlgorithm()
+    {
+
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// Возвращает первую строки с индексом <paramref name="index"/> или <see langword="null"/>, если длина <paramref name="input"/> меньше либо равна <paramref name="index"/>.
+    /// </summary>
+    /// <param name="input">
+    /// <inheritdoc/>
+    /// </param>
+    /// <param name="index">
+    /// Индекс.
+    /// </param>
+    /// <returns>
+    /// Первая буква строки или <see langword="null"/>, 
+    /// если длина <paramref name="input"/> меньше либо равна <paramref name="index"/>.
+    /// </returns>
+    /// <exception cref="NullReferenceException">
+    /// Выбрасывается, когда <paramref name="input"/> является <see langword="null"/>.
+    /// </exception>
+    public char? Run(string input, int index)
+    {
+        _ = ThrowHelper.ThrowIfNull(input);
+
+        if (input.Length <= index)
+        {
+            return null;
+        }
+
+        return input[index];
+    }
+}
